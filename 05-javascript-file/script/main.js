d3884f538cb13d8ac6101bab4ff7524126e0a583
Tickere <pablo.blumer@gmail.com>
2024-06-07 09:09:22 +0200

    testing javascript alert message

--- 05-javascript-file/script/main.js
+++ 05-javascript-file/script/main.js
@@ -1,1 +1,26 @@
+let message;
+message = "Hello!";
+
+alert(message);
+
 console.log("Hello Universe!");
+
+const classroom = {
+  students: ["Alice", "Bob", "Charlie"],
+  teacher: {
+    name: "Mrs. Smith",
+    subject: "Math",
+    age: 35,
+  },
+  location: "Room 101",
+  capacity: 30,
+  isFull: false,
+};
+
+let name = "John";
+
+// embed a variable
+alert(`Hello, ${name}!`); // Hello, John!
+
+// embed an expression
+alert(`the result is ${1 + 2}`); // the result is 3
